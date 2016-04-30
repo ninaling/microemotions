7b47a0d6b35c7ff077d7da4224dedaa9a5ac3f5c
thetoothless <ninaling@ucla.edu>
2016-04-30 08:56:45 -0700

    aight emotions api works

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+var oxford = require('project-oxford');
+var client = new oxford.Client('41106ff66b604242ab632a658b2d2db3');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+client.emotion.analyzeEmotion({
+    path: 'imgtests/donald-trump.png',
+}).then(function (response) {
+    console.log("anger: " + response[0].scores.anger);
+    console.log("contempt: " + response[0].scores.contempt);
+    console.log("disgust: " + response[0].scores.disgust);
+});
