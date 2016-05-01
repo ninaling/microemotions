0afa181a9112a37b211f3da118599072fe6bc19c
thetoothless <ninaling@ucla.edu>
2016-05-01 09:23:15 -0700

    added josh's answers

--- public/answers.js
+++ public/answers.js
@@ -1,43 +1,43 @@
 var items=[
     {
-        title: "what's your favourite colour?",
-        message: "lime green"
+        title: "If you could invite three people to dinner who would they be (alive only)?",
+        message: "A: Dwayne \"The Rock\" Johnson, Barack Obama, and Gordan Ramsay"
     },
     {
-        title: "what's your favourite animal?",
-        message: "llama/doge"
+        title: "Best movie you've ever seen?",
+        message: "A: Nacho Libre"
     },
     {
-        title: "hyphens or underscores as delimiters?",
-        message: "underscores, hand down"
+        title: "If Batman fought all 4 ninja turtles at the same time who would win?",
+        message: "A: Ninja turtles by far"
     },
     {
-        title: "test question 4",
-        message: "test answer 4"
+        title: "If you could be one animal what would it be?",
+        message: "A: crocodile"
     },
     {
-        title: "test question 5",
-        message: "test answer 5"
+        title: "Would you rather spend a week stranded in the jungle or the desert?",
+        message: "A: the desert, way too many giant bugs in the jungle"
     },
     {
-        title: "test question 6",
-        message: "test answer 6"
+        title: "If you could only drink one thing other than water for the rest of your life what would it be?",
+        message: "beer"
     },
     {
-        title: "test question 7",
-        message: "test answer 7"
+        title: "Favorite sport to play or watch?",
+        message: "A: Rugby to play and football to watch"
     },
     {
-        title: "test question 8",
-        message: "test answer 8"
+        title: "Least favorite genre of music?",
+        message: "A: heavy trap music"
     },
     {
-        title: "test question 9",
-        message: "test answer 9"
+        title: "Rank the parts of a triathlon (swimming, biking, and running) in order from favorite to least favorite",
+        message: "A: biking, swimming, running"
     },
     {
-        title: "test question 10",
-        message: "test answer 10"
+        title: "Would you rather never be able to access the internet again and live in LA or live on a remote island isolated with full access to the internet?",
+        message: "A: LA with no internet, you can't replace actually interacting with other humans"
     }
 ];
 
